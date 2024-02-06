b5d4fc5e5d3b2cd14452d73ff02eb97c0369a57d
Arniel C <aceba1@outlook.com>
2024-02-06 11:43:21 -0500

    initialize svelte-kit
    `npm create svelte@latest`

---
+++ docs/_app/immutable/entry/start.Vytv01Kt.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.vGf0cB2L.js";export{t as start};
---
+++ svelte.config.js
@@ -0,0 +1,22 @@
+import adapter from '@sveltejs/adapter-static';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	compilerOptions: {
+		generate: "dom",
+	},
+	kit: {
+		// https://kit.svelte.dev/docs/adapters
+		adapter: adapter({
+			pages: "docs",
+			assets: "docs",
+			fallback: '404.html',
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
+};
+
+export default config;
